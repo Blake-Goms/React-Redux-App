09687a8344e981c57cc7f379a0b0e974348f3dbe
Blake Goms <blakegoms@gmail.com>
2019-07-25 13:26:17 -0700

    initital commit

---
+++ bf4/src/reducer/index.js
@@ -0,0 +1,13 @@
+
+export const initialState = {
+    error: '',
+    isFetching: false,
+    playerInfo: null,
+} 
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
